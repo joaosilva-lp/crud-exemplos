ab57be3b3cc12c3e02c1e7c5777372d2eaa2b59c
j silva2 <joaosilva@mondegovidro.local>
2024-01-04 14:53:53 +0000

    doing some queries

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, setDoc, doc, updateDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, setDoc, doc, updateDoc, deleteDoc, deleteField, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCI5C6mqFFtIkZF3tAz9fI4NVLYsplb9oA",
@@ -21,16 +21,16 @@ const db = getFirestore(app);
 // Add a document to the "users" collection
 (async () => {
   try {
-    const docRef = await addDoc(collection(db, "clients"), {
-      name: "Mondegovidro - Soluções em Vidro Lda",
+    const docRef = await addDoc(collection(db, "city"), {
+      name: "Coimbra",
       country: "Portugal"
     });
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
-})();*/
-
+})();
+*/
 /*
  //setDoc()
 (async () => {
@@ -61,14 +61,69 @@ const db = getFirestore(app);
 })();
 */
 
+/*
+#####UDDATE######
 (async()=>{
   try {
     const cityDocRef = doc(db, "cities", "9GNz3ANhQAm0T6Wyj59H");
     await updateDoc(cityDocRef, {
-      river: "Sado"
-    }, {merge:true})
+      river2: "Sado",
+      bairros: {
+        centro: {
+          1: "vila mariana",
+          2: "vila clementino",
+          3: "paulista",
+          4: "brigadeiro"
+        },
+        baixa: "baixa"
+      }
+    })
     console.log("Registo criado com sucesso")
   } catch (error) {
       console.log(error)
   }
+})();*/
+
+
+/*
+//deleteDoc() Apaga toda a collection
+(async()=>{
+  try {
+    const cityDocRef = doc(db, "city", "jkY8DnsN7054d0whPT1w");
+    await deleteDoc(cityDocRef)
+    console.log("The entire Document was deleted")
+  } catch (error) {
+      console.log(error)
+  }
 })();
+*/
+
+/*
+//deleteField()
+(async()=>{
+  try {
+    const cityDocRef = doc(db, "city", "jkY8DnsN7054d0whPT1w");
+    await updateDoc(cityDocRef, {
+      country: deleteField()
+    })
+    console.log("Registo apagado com sucesso")
+  } catch (error) {
+      console.log(error)
+  }
+})();
+*/
+/*
+//Get Document Data By ID From a Collection Using getDoc()
+(async()=>{
+  try {
+    const cityDocRef = doc(db, "city", "9Mbj98LfqRSIDtzzzPTp");
+    const docData = await getDoc(cityDocRef)
+
+    console.log(docData.data())
+  } catch (error) {
+      console.log(error)
+  }
+})();
+//output { name: 'Braga', country: 'Portugal' }
+
+*/
