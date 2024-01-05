a5df16d1876899c50b7a7ba638c10d1b259356f0
j silva2 <joaosilva@mondegovidro.local>
2024-01-05 08:36:53 +0000

    open and close modal form

--- firebase/app.js
+++ firebase/app.js
@@ -1,1 +1,23 @@
 
+const addBtn = document.querySelector(".add-btn");
+const modalOverlay = document.getElementById("modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+const addButtonPressed = () => {
+    modalOverlay.style.display="flex";
+}
+
+const closeButtonPressed = ()=>{
+    modalOverlay.style.display = "none";
+}
+
+const hideModal = (e) =>{
+    if(e.target===e.currentTarget){
+        modalOverlay.style.display = "none"
+    }
+}
+
+
+addBtn.addEventListener("click", addButtonPressed);
+closeBtn.addEventListener("click", closeButtonPressed);
+modalOverlay.addEventListener("click", hideModal);
