ae0ad5e9cf22fde8efd943ee825aa935a9154b46
j silva2 <joaosilva@mondegovidro.local>
2024-01-05 17:34:58 +0000

    send

--- firebase/app.js
+++ firebase/app.js
@@ -1,3 +1,18 @@
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    onSnapshot,
+    doc,
+    updateDoc,
+    deleteDoc,
+  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  
+  const db = getFirestore();
+  const dbRef = collection(db, "agenda");
+
+
+//MODAL
 
 const addBtn = document.querySelector(".add-btn");
 const modalOverlay = document.getElementById("modal-overlay");
@@ -21,3 +36,102 @@ const hideModal = (e) =>{
 addBtn.addEventListener("click", addButtonPressed);
 closeBtn.addEventListener("click", closeButtonPressed);
 modalOverlay.addEventListener("click", hideModal);
+
+
+
+//FORM VALIDATION
+
+const saveBtn = document.querySelector(".save-btn");
+
+const error = {};
+
+const firstname = document.getElementById("fistname");
+
+const lastname = document.getElementById("lastname");
+
+const email = document.getElementById("email");
+
+const age = document.getElementById("age");
+
+const phone = document.getElementById("phone");
+
+
+const saveButtonPressed = async() =>{
+    //console.log(firstname.value)
+    checkRequired([firstname,lastname,email,age,phone]);
+    checkEmail(email);
+    checkInputLength(age,2);
+    checkInputLength(phone,10);
+    showErrorMessages();
+    
+    if(Object.keys(error).length === 0){
+        await addDoc(dbRef, {
+            firstname: firstname.value,
+            lastname: lastname.value,
+            age: age.value,
+            phone: phone.value,
+            email: email.value
+        });
+    };
+}
+
+const checkRequired = (inputArray) => {
+    inputArray.forEach((input) => {
+      if (input.value.trim() === "") {
+        setErrorMessage(input, input.id + " is empty");
+      } else {
+        deleteErrorMessage(input);
+      }
+    });
+  };
+
+  const setErrorMessage = (input, message) => {
+    if (input.nodeName === "INPUT") {
+      error[input.id] = message;
+      input.style.border = "1px solid red";
+    } else {
+      error[input] = message;
+    }
+  };
+
+const deleteErrorMessage = (input) => {
+    delete error[input.id];
+    input.style.border = "1px solid green";
+  };
+
+const checkInputLength = (input, num) => {
+    if (input.value.trim() !== "") {
+      if (input.value.trim().length === num) {
+        deleteErrorMessage(input);
+      } else {
+        setErrorMessage(input, input.id + ` must be ${num} digits`);
+      }
+    }
+  };
+
+const checkEmail = (input) => {
+    if (input.value.trim() !== "") {
+      const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (re.test(input.value.trim())) {
+        deleteErrorMessage(input);
+      } else {
+        setErrorMessage(input, input.id + " is invalid");
+      }
+    }
+  };
+
+
+const showErrorMessages = ()=>{
+    const errorLabel = document.getElementById("error-label");
+    errorLabel.innerHTML = "";
+    for(const key in error){
+        const li = document.createElement("li");
+        li.innerHTML = error[key];
+        li.style.color = "red"
+        errorLabel.appendChild(li);
+    }
+}
+
+
+saveBtn.addEventListener("click", saveButtonPressed);
---
+++ firebase/firebase-sdk.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyC27eTeZPvI1dIkAa5wJGSwZGKZ59-oIJw",
+
+    authDomain: "agenda-448e3.firebaseapp.com",
+
+    projectId: "agenda-448e3",
+
+    storageBucket: "agenda-448e3.appspot.com",
+
+    messagingSenderId: "516331053252",
+
+    appId: "1:516331053252:web:7d2397c3864a83b1222991"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
